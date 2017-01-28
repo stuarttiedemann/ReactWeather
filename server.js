f3dbd71333810d343d0c373e8a4ecaf734c2d126
Stuart Tiedemann <tiedemannstuart@gmail.com>
2017-01-28 12:25:30 -0600

    Fix issue with localhost

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const PORT = process.env.PORT || 3000;
 
 // For open Weather API because it doesn't use https:
 app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 });
 
