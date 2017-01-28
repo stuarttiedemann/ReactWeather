ba4b7b1334d2618341d57d1584b4380e86492df5
Stuart Tiedemann <tiedemannstuart@gmail.com>
2017-01-28 12:06:23 -0600

    Update code for Heroku deployment
    Update package.json so that heroku knows how to start app and Server.js
    so that openWeather API can use http instead of trying to use https
    which it doesn’t support.

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ var express = require('express');
 
 // Create our app 
 var app = express();
+// Environment variable for heroku
+const PORT = process.env.PORT || 3000;
+
+// For open Weather API because it doesn't use https:
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 // Tell which folder to serve 
 app.use(express.static('public'));
 
 // Start the server 
-app.listen(3000, function() {
-  console.log('Express server is running on port 3000');
+app.listen(PORT, function() {
+  console.log('Express server is running on port ' + PORT);
 });
