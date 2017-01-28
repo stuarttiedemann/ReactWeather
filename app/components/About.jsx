543f73abe56bba1d71e177e77d67a1c769f11eb2
Stuart Tiedemann <tiedemannstuart@gmail.com>
2017-01-28 12:45:01 -0600

    Update About copy and practice deploying to Heroku.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,10 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>The New About Component</h3>
+    <div>
+      <h3>About</h3>
+      <p>Welcome to the About page</p>
+    </div>
   )
 };
 
